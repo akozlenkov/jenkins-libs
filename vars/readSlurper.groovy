a029b7a6e26ac69d50b89da7bb5bf8a68abc9366
Andrey Kozlenkov <a.kozlenkov@hh.ru>
2021-02-19 14:41:42 +0300

    added slurper

--- vars/readSlurper.groovy
+++ vars/readSlurper.groovy
@@ -1,8 +1,9 @@
 import groovy.json.JsonSlurper
 
 def call(String path) {
-    def inputFile = new File(path);
-    return inputFile.text;
+    //def inputFile = new File(path);
+    //return inputFile.text;
     //return new JsonSlurper().parseText(inputFile.text);
+    return new ConfigSlurper().parse(new URL("file:$path"));
 }
 
