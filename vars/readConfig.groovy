2ea80655a3cbb1fc421ddc821f8f6ba72d087755
Andrey Kozlenkov <andrey.kozlenkov@gmail.com>
2022-03-06 23:17:46 +0300

    config reader

--- vars/readConfig.groovy
+++ vars/readConfig.groovy
@@ -1,13 +1,13 @@
-@Grab('org.codehaus.groovy:groovy-all:3.0.9')
-@Grab('org.codehaus.groovy:groovy-yaml:3.0.9')
-import groovy.yaml.YamlSlurper
-import groovy.text.GStringTemplateEngine
+// @Grab('org.codehaus.groovy:groovy-all:3.0.9')
+// @Grab('org.codehaus.groovy:groovy-yaml:3.0.9')
+// import groovy.yaml.YamlSlurper
+// import groovy.text.GStringTemplateEngine
 
-def createConfig = { file ->
-    def config = new File(file).getText()
-    return new YamlSlurper().parseText(engine.createTemplate(config).make(new YamlSlurper().parseText(config)).toString()) as Map
-}
+// def createConfig = { file ->
+//     def config = new File(file).getText()
+//     return new YamlSlurper().parseText(engine.createTemplate(config).make(new YamlSlurper().parseText(config)).toString()) as Map
+// }
 
 def call(String path) {
-    return createConfig(path)
+    return new File(file).getText()
 }
